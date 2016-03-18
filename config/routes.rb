f34169a66c38944773c07df32072fb9ab844bafa
Sarp Solakoglu <sosolakoglu@gmail.com>
2016-03-18 16:55:35 +0000

    create article

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   
   get 'index', to: 'welcome#index'
 
+  resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
