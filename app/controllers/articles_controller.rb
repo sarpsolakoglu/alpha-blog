3e9b97b2af5e40b4eaf8d2d324002239174c4c26
Sarp Solakoglu <sosolakoglu@gmail.com>
2016-03-18 17:14:02 +0000

    showing article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,8 +5,16 @@ class ArticlesController < ApplicationController
   def create
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article was succesfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @article = Article.find(params[:id])
   end
   
   private
@@ -14,7 +22,4 @@ class ArticlesController < ApplicationController
     params.require(:article).permit(:title, :description)
   end
   
-  def show
-    
-  end
 end
